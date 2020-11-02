cdcd1380a7605c3a415ec4880c0f6b634569a937
Andrew Hawkes <adw.hwk@gmail.com>
2020-11-03 08:46:49 +1100

    Beginning UI

---
+++ src/js/app.js
@@ -0,0 +1,94 @@
+let state = {};
+
+
+// DATA CONTROLLER
+
+const searchController = (() => {
+
+
+    return {
+        search: async function(query) {
+
+            state.query = query;
+
+            const baseURL = 'https://data.gov.au/data/api/3/action/datastore_search?resource_id=cb7e4eb5-ed46-4c6c-97a0-4532f4479b7d&q=';
+
+            try {
+                const result = await fetch(baseURL + query);
+                const data = await result.json();
+                state.search = data.result.records;
+            } catch (error) { alert('There was an error with your search') };
+        }
+    }
+})();
+
+
+// UI CONTROLLER
+
+const UIController = (() => {
+
+    const DOMStrings = {
+        searchForm: '#searchForm',
+        searchInput: '#searchInput',
+        searchButton: '#searchButton',
+        clearButton: '#clearButton'
+    }
+
+
+    return {
+        getDOMStrings: function() {
+            return DOMStrings;
+        },
+
+        clearInput: function() {
+            document.querySelector(DOMStrings.searchInput).value = '';
+        },
+
+        getInput: function() {
+            return document.querySelector(DOMStrings.searchInput).value;
+        },
+
+        setPlaceholder: function(value) {
+            document.querySelector(DOMStrings.searchInput).placeholder = value;
+        }
+    }
+
+})();
+
+
+// APP CONTROLLER
+
+const controller = ((searchCtrl, UICtrl) => {
+
+    const DOM = UICtrl.getDOMStrings();
+
+    const setupEventListeners = () => {
+
+        document.querySelector(DOM.searchForm).addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const input = UICtrl.getInput();
+
+            if (input) {
+                searchCtrl.search(input);
+                UICtrl.setPlaceholder(input);
+                UICtrl.clearInput();
+            }
+
+        });
+
+        document.querySelector(DOM.clearButton).addEventListener('click', () => {
+            document.querySelector(DOM.searchInput).value = '';
+        })
+
+    };
+
+    return {
+        init: function() {
+            setupEventListeners();
+        }
+    }
+
+})(searchController, UIController);
+
+controller.init();
