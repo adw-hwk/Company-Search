6b93ae174a20125821b9bc68d56eabe6beac40fa
Andrew Hawkes <adw.hwk@gmail.com>
2020-11-08 22:39:33 +1100

    Working version, added loading spinner

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './scss/styles.scss';
+// import '../node_modules/bootstrap/js/src/index'
+import './js/app';
--- src/js/app.js
+++ src/js/app.js
@@ -13,6 +13,7 @@ const searchController = (() => {
             try {
                 const result = await fetch(baseURL + query);
                 const data = await result.json();
+                console.log(data.result);
                 search.results = data.result.records;
 
                 return data.result.records;
@@ -21,51 +22,42 @@ const searchController = (() => {
             }
         },
 
-        getSearch: () => {
-            return search;
-        },
+        sortResultsByDate: (results, oldestToNewest = true) => {
+            return results.sort((cur, next) => {
+                const curDate = cur["Date of Registration"];
+                const nextDate = next["Date of Registration"];
 
-        stringToDate: (dateString) => {
-            // expected dateString format DD/MM/YYYY
-            // crude validation of input date string, improve later
-            if (
-                dateString.length === 10 &&
-                dateString[2] === "/" &&
-                dateString[5] === "/"
-            ) {
+                // date object for current result
+                const a = {
+                    day: Number(curDate.slice(0, 2)),
+                    month: Number(curDate.slice(3, 5)),
+                    year: Number(curDate.slice(6)),
+                };
 
-                try {
-                    const dateArr = dateString.split("/");
+                // date object for next result
+                const b = {
+                    day: Number(nextDate.slice(0, 2)),
+                    month: Number(nextDate.slice(3, 5)),
+                    year: Number(nextDate.slice(6)),
+                };
 
-                    // required format for Date() - MM-DD-YYYY (why? idk, because America I suppose)
-                    // MM - dateArr[1], MM - dateArr[0], YYYY - dateArr[2]
-                    const regDate = new Date(`${dateArr[1]}-${dateArr[0]}-${dateArr[2]}`);
+                let sortCondition;
 
-                    return regDate;
-                } catch (e) {
-                    console.warn(e);
-                    return false;
+                if (oldestToNewest) {
+                    sortCondition =
+                        a.year > b.year ||
+                        (a.year === b.year && a.month > b.month) ||
+                        (a.year === b.year && a.month === b.month && a.day > b.day);
+                } else {
+                    sortCondition =
+                        a.year < b.year ||
+                        (a.year === b.year && a.month < b.month) ||
+                        (a.year === b.year && a.month === b.month && a.day < b.day);
+
                 }
+                return sortCondition ? 1 : -1;
 
-            } else {
-                console.warn('Date string format doesn\'t match DD/MM/YYYY');
-                return false;
-            }
-        },
-
-        // searchControllers own stringToDate function (above) is passed as converterFunc argument in the form submit event listener inside app controller
-        sortResultsByDate: (results, converterFunc, oldestToNewest = true) => {
-
-            if (oldestToNewest) {
-                return results.sort((cur, next) => {
-                    return converterFunc(cur["Date of Registration"]).getTime() > converterFunc(next["Date of Registration"]).getTime() ? 1 : -1
-                });
-            } else {
-                return results.sort((cur, next) => {
-                    return converterFunc(cur["Date of Registration"]).getTime() < converterFunc(next["Date of Registration"]).getTime() ? 1 : -1
-                });
-            }
-
+            });
         },
     };
 })();
@@ -77,9 +69,68 @@ const UIController = (() => {
         searchForm: "#searchForm",
         searchInput: "#searchInput",
         searchButton: "#searchButton",
-        clearButton: "#clearButton",
         resultsList: "#resultsList",
+        spinLoader: "#spinLoader"
     };
+
+    const loader = () => {
+        let el = document.createElement('div');
+        el.classList.add('spinLoader');
+
+        return el;
+    };
+
+    const card = {
+        // - Company Name
+        // - Current Name
+        skeleton: () => {
+            let card = document.createElement('div');
+            card.classList.add('result-card');
+            let cardTop = document.createElement('div');
+            cardTop.classList.add('card-top');
+            let cardBottom = document.createElement('div');
+            cardBottom.classList.add('card-bottom');
+
+            card.appendChild(cardTop);
+            card.appendChild(cardBottom);
+
+            return card;
+        },
+        top: (record) => {
+            let HTML = '';
+
+            if (record["Company Name"] !== null && record["Company Name"].length) {
+                HTML += `<h2 class="comp-name">${record["Company Name"]} ${card.googleIcon(record)}</h2>`;
+            }
+
+            if (record["Current Name"] !== null && record["Current Name"].length) {
+                HTML += `<small>Current Name:</small>`;
+                HTML += `<h4 class="curr-name">${record["Current Name"]}</h4>`
+            }
+
+            return HTML;
+        },
+
+        detailsText: (record, field) => {
+
+            let HTML = '';
+
+            if (record[field] && record[field] !== '0') {
+                HTML += `<div class="info-group"><div class="field">${field}</div><div class="value">${record[field]}</div></div>`
+            }
+
+            return HTML;
+
+        },
+
+        googleIcon: (record) => {
+
+            return `<a target="_blank" style="margin-left: auto;" href="https://google.com/search?q=${record["Company Name"]}"><img class="google-icon" src="img/googleSearchIcon.png" height=40 width=40></a>`;
+
+        }
+    };
+
+
 
     return {
         getDOMStrings: () => {
@@ -97,16 +148,60 @@ const UIController = (() => {
         setPlaceholder: (value) => {
             document.querySelector(DOMStrings.searchInput).placeholder = value;
         },
+        toggleLoader: () => {
+
+            const resList = document.querySelector(DOMStrings.resultsList);
+            let spinner = document.querySelector('.spinLoader');
+
+            if (resList.contains(spinner)) {
+                // remove loader
+                resList.removeChild(spinner);
+            } else {
+                // add loader
+                spinner = loader();
+                resList.appendChild(spinner);
+            }
+        },
         renderResults: (results) => {
             const resultsList = document.querySelector(DOMStrings.resultsList);
+
+            const infoKeys = [
+                "Date of Registration",
+                "ABN",
+                "ACN",
+                "Current Name Start Date",
+                "Previous State of Registration",
+                "Status",
+                "Class",
+                "Sub Class",
+                "Type"
+            ];
 
             resultsList.innerHTML = "";
 
             results.forEach((result) => {
-                let li = `<li class="result-card"><h2 class="company-name">${result["Company Name"]}<a target="_blank" href="https://google.com/search?q=${result["Company Name"]}"><i class="fab fa-google"></i></a></h2><div>Date of Registration: <span class="bold">${result["Date of Registration"]}</span></div><div>ABN: <span class="bold">${result["ABN"]}</span></div></li>`;
-                resultsList.innerHTML += li;
+
+                let shell = card.skeleton();
+
+                const cardTop = shell.querySelector('.card-top');
+                const cardBottom = shell.querySelector('.card-bottom');
+
+                cardTop.innerHTML = card.top(result);
+
+                infoKeys.forEach(key => {
+                    cardBottom.innerHTML += card.detailsText(result, key);
+                });
+
+                resultsList.insertAdjacentElement('beforeend', shell);
+
             });
         },
+        clearResults: () => {
+            document.querySelector(DOMStrings.resultsList).innerHTML = '';
+        },
+        toTop: () => {
+            window.scrollTo(0, 0);
+        }
     };
 })();
 
@@ -124,18 +219,18 @@ const controller = ((searchCtrl, UICtrl) => {
                 const input = UICtrl.getInput();
 
                 if (input) {
+                    UICtrl.toTop();
+                    UICtrl.clearResults();
+                    UICtrl.toggleLoader();
                     const results = await searchCtrl.search(input);
                     UICtrl.setPlaceholder(input);
                     UICtrl.clearInput();
 
-                    const sortedResults = searchCtrl.sortResultsByDate(results, searchCtrl.stringToDate);
+                    const sortedResults = searchCtrl.sortResultsByDate(results);
+                    UICtrl.toggleLoader();
                     UICtrl.renderResults(sortedResults);
                 }
             });
-
-        document.querySelector(DOM.clearButton).addEventListener("click", () => {
-            UICtrl.clearInput();
-        });
     };
 
     return {
